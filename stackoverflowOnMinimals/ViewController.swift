409752eacb6b84d3dca7630169e86a0e82e7dd52
Vladimir Drozdin <rindzorcoop@gmail.com>
2020-03-29 13:08:43 +0300

    Initial Commit

---
+++ stackoverflowOnMinimals/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stackoverflowOnMinimals
+//
+//  Created by  mac on 3/29/20.
+//  Copyright © 2020 Vladimir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
